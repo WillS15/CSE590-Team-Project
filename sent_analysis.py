3084ca7dc5662cf1cc3b76835c2c7b2e6ebc634b
Buffal0-Bill <colton7797@gmail.com>
2023-11-11 14:20:42 -0500

    Text data has been tokenized to PyTorch focused Tensors using a pretrained BERT-cased model

--- sent_analysis.py
+++ sent_analysis.py
@@ -4,7 +4,10 @@ import pandas as pd
 import torch
 import toolbox as tb
 
+SEED = 1024
+
 df = tb.read_data('IMDB_Dataset.csv')
+df = tb.tokenize_data(df)
 
 breakpoint()
 
--- toolbox.py
+++ toolbox.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import transformers as tran
 
 def read_data(filename):
     '''
@@ -14,3 +15,16 @@ def read_data(filename):
     
     return df
     
+def tokenize_data(df):
+    '''
+    Loads a pretrained BERT cased model
+    Tokenizes the text data and replaces with PyTorch specific Tensors: ['input_ids', 'token_type_ids', 'attention_mask']
+    Each Tensor can be accessed by df[<dataset_index>][<which_tensor>], ex. "df[0]['input_ids']"
+    '''
+    
+    tokenizer = tran.AutoTokenizer.from_pretrained("bert-base-cased")
+
+    df['text'] = df['text'].apply(str)
+    df['text'] = df['text'].apply(tokenizer, padding=True, truncation=True, return_tensors="pt")
+
+    return df
