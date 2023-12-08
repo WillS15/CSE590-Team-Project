d2266c2072f3c192aa8d2b14d3d496290d29c1dc
Buffal0-Bill <colton7797@gmail.com>
2023-12-07 19:57:51 -0500

    Dataset now works as a PyTorch dataset that works with a PyTorch DataLoader

--- sent_analysis.py
+++ sent_analysis.py
@@ -2,15 +2,18 @@
 import pandas as pd
 import toolbox as tb
 
+#Data
 data_split = 0.8
+batch_size = 100
 
+#Model
 embedding_dim = 64
 hidden_dim = 256
 num_layers = 2
 
-#Remake init to split and shuffle
 data = tb.SRNN_Dataset('IMDB_Dataset.csv', data_split)
 model = tb.SentimentRNN(data.vocab_size, embedding_dim, hidden_dim, num_layers)
+dataloader = tb.DataLoader(data, shuffle=True, batch_size=batch_size)
 breakpoint()
 
 print('Yay')
--- toolbox.py
+++ toolbox.py
@@ -1,12 +1,26 @@
 import pandas as pd
 import torch
 import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 #from nltk.tokenize import word_tokenize
 #import nltk
 from transformers import BertTokenizerFast
 
-class SRNN_Dataset():
-    def __init__(self, filename, split_val):
+class SRNN_Dataset(Dataset):
+    """
+    Takes the entire dataset from a csv and performs preprocessing and splitting.
+    After splitting, this class works as a PyTorch dataset, intended to be used with a DataLoader.
+    The required methods for a DataLoader will work with the training data only.
+    Test data will be stored in this class but will need to be accessed directly.
+    """
+    
+    def __init__(self, filename, split_val=0.8):
+        """
+        Arguments:
+        
+        filename (string)
+        split_val (float): Percentage of train test split
+        """
 
         print('Reading and Preprocessing ... ', end='', flush=True)
         
@@ -43,7 +57,8 @@ class SRNN_Dataset():
         
         #BERT Tokenizer & Encoding
         tokenizer = BertTokenizerFast.from_pretrained("bert-base-cased")
-        df = pd.concat([df['text'], df['text'].apply(tokenizer.encode, padding=True, truncation=True, return_tensors="pt").rename('tokens'), df['label']], axis=1)
+        df = pd.concat([df['text'], df['text'].apply(tokenizer.encode, padding="max_length", truncation=True, return_tensors="pt").rename('tokens'), df['label']], axis=1)
+        df.tokens = df.tokens.map(torch.flatten)
 
         print('Done')
         print('Shuffle and Split ... ', end='', flush=True)
@@ -54,17 +69,29 @@ class SRNN_Dataset():
 
         print('Done')
 
+        self.split_val = split_val
         self.train = train
         self.test = test
         self.tokenizer = tokenizer
         self.vocab_size = tokenizer.vocab_size
 
     def __len__(self):
-        return len(self.train.index) + len(self.test.index)
+        return len(self.train.index)
+
+    def __getitem__(self, ind):
+        dic = {"text": self.train.iloc[ind].text, "tokens": self.train.iloc[ind].tokens, "label": self.train.iloc[ind].label}
+        return self.train.iloc[ind].to_dict()
 
 class SentimentRNN(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, num_layers):
         super(SentimentRNN, self).__init__()
+
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+            print("GPU is available")
+        else:
+            self.device = torch.device("cpu")
+            print("GPU not available, CPU used")
 
         self.emb = nn.Embedding(vocab_size, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers, batch_first=True)
