3d0d24cd786e8ccebdddae52cf055d199b2cbada
Buffal0-Bill <colton7797@gmail.com>
2023-12-09 00:27:03 -0500

    Model can run and is spitting out good results. Now on to graphing.

--- sent_analysis.py
+++ sent_analysis.py
@@ -2,16 +2,6 @@
 import pandas as pd
 import toolbox as tb
 
-#Data
-data_split = 0.8
-batch_size = 100
-
-#Model
-embedding_dim = 64
-hidden_dim = 256
-num_layers = 2
-dropout_prob = 0.3
-
 if tb.torch.cuda.is_available():
     device = tb.torch.device("cuda")
     print("GPU is available")
@@ -19,32 +9,87 @@ else:
     device = tb.torch.device("cpu")
     print("GPU not available, CPU used")
 
-data = tb.SRNN_Dataset('IMDB_Dataset.csv', data_split)
-#model = tb.SentimentRNN(data.vocab_size, embedding_dim, hidden_dim, num_layers, dropout_prob).to(device)
-model = tb.SentimentRNN(data.vocab_size, embedding_dim, hidden_dim, num_layers, dropout_prob)
-dataloader = tb.DataLoader(data, shuffle=True, batch_size=batch_size)
+# Data
+data_split = 0.8
+batch_size = 50
 
-#Training
-clips = 5
-#epochs = 5
-epochs = 1
-lr = 0.001
+training_data, testing_data, vocab_size = tb.read_preprocess_split_data('IMDB_Dataset.csv', data_split)
+training_data, testing_data = tb.SRNN_Dataset(training_data), tb.SRNN_Dataset(testing_data)
+
+training_dataloader = tb.DataLoader(training_data, shuffle=True, batch_size=batch_size)
+testing_dataloader = tb.DataLoader(testing_data, shuffle=True, batch_size=batch_size)
+
+# Model
+embedding_dim = 512 #BERT Max Sequence Length
+hidden_dim = 128
+no_layers = 2
+dropout_prob = 0.3 if no_layers > 1 else 0
+
+model = tb.SentimentRNN(vocab_size, embedding_dim, hidden_dim, no_layers, dropout_prob).to(device)
+print(model)
+
+# Learning Params
+clip = 5
+epochs = 11
+learning_rate = 0.005
 loss_function = tb.nn.BCELoss()
-optimizer = tb.torch.optim.Adam(model.parameters(), lr=lr)
+optimizer = tb.torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+# Run History Params
+training_losses = tb.np.empty(epochs)
+training_accs = tb.np.empty(epochs)
+testing_losses = tb.np.empty(epochs)
+testing_accs = tb.np.empty(epochs)
+
+# Running
 for epoch in range(epochs):
-    losses = []
-    acc = 0.0
-    model.train()
+    losses = tb.np.array([])
+    accs = 0
     hidden_state = model.init_hidden(batch_size, device)
-    
-    for tokens, labels in dataloader:
-        #tokens, labels = tokens.to(device), labels.to(device)
+
+    print(f"Epoch {epoch+1}")
+    model.train()    
+    for tokens, labels in training_dataloader:
+        tokens, labels = tokens.to(device), labels.to(device)
 
         hidden_state = tuple([each.data for each in hidden_state])
         model.zero_grad()
         output, hidden_state = model(tokens, hidden_state, batch_size)
-        breakpoint()
-        break
 
+        loss = loss_function(output.clamp(0, 1), labels.float())
+        loss.backward()
+        losses = tb.np.append(losses, [loss.item()])
+
+        accs += tb.calc_accuracy(output, labels)
+
+        tb.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        optimizer.step()
+
+    training_accs[epoch] = accs / len(training_dataloader.dataset)        
+    training_losses[epoch] = tb.np.mean(losses)
+
+    print(f"Training: Accuracy: {training_accs[epoch]*100}, Loss: {training_losses[epoch]}")
+
+    losses = tb.np.array([])
+    accs = 0
+    hidden_state = model.init_hidden(batch_size, device)
+
+    model.eval()
+    for tokens, labels in testing_dataloader:
+        tokens, labels = tokens.to(device), labels.to(device)
+
+        hidden_state = tuple([each.data for each in hidden_state])
+        output, hidden_state = model(tokens, hidden_state, batch_size)
+
+        loss = loss_function(output.clamp(0, 1), labels.float())
+        losses = tb.np.append(losses, [loss.item()])
+
+        accs += tb.calc_accuracy(output, labels)
+
+    testing_accs[epoch] = accs / len(testing_dataloader.dataset)        
+    testing_losses[epoch] = tb.np.mean(losses)
+        
+    print(f"Testing: Accuracy: {testing_accs[epoch]*100}, Loss: {testing_losses[epoch]}")
+
+breakpoint()
 print('Yay')
--- toolbox.py
+++ toolbox.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
@@ -9,96 +10,96 @@ from transformers import BertTokenizerFast
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-class SRNN_Dataset(Dataset):
+def calc_accuracy(preds, labels):
+    preds = torch.round(preds)
+    return torch.sum(preds == labels).item()
+
+def read_preprocess_split_data(filename, split_val=0.8):
     """
-    Takes the entire dataset from a csv and performs preprocessing and splitting.
-    After splitting, this class works as a PyTorch dataset, intended to be used with a DataLoader.
-    The required methods for a DataLoader will work with the training data only.
-    Test data will be stored in this class but will need to be accessed directly.
-    """
-    
-    def __init__(self, filename, split_val=0.8):
-        """
         Arguments:
         
         filename (string)
         split_val (float): Percentage of train test split
+    """
+    
+    print('Reading and Preprocessing ... ', end='', flush=True)
+        
+    df = pd.read_csv(filename)
+    df = df.rename(columns={'review': 'text', 'sentiment': 'label'})
+
+    #Replace 'positive' and 'negative' labels with 1 or 0 respectively
+    df['label'] = df['label'].map(lambda x: 1 if x=='positive' else 0)
+    
+    print('Done')
+    print('Cleaning ... ', end='', flush=True)
+
+    def clean_sent(sent):
+        import re
+        #HTML Tags
+        sent = re.sub(r"(<.*?>){1,2}", " ", sent)
+        
+        #Nonword characters
+        #sent = re.sub(r"[^\w\s]", "", sent)
+        
+        #Removes repeating whitespace
+        sent = re.sub(r"\s+", " ", sent)
+        
+        return sent
+            
+    df['text'] = df['text'].apply(clean_sent)
+        
+    print('Done')
+    print('Tokenizing ... ', end='', flush=True)
+
+    #BERT Tokenizer & Encoding
+    tokenizer = BertTokenizerFast.from_pretrained("bert-base-cased")
+    df = pd.concat([df['text'], df['text'].apply(tokenizer.encode, padding="max_length", truncation=True, return_tensors="pt").rename('tokens'), df['label']], axis=1)
+    df.tokens = df.tokens.map(torch.flatten)
+
+    print('Done')
+    print('Splitting ... ', end='', flush=True)
+
+    split_index = round(len(df)*split_val)
+    train, test = df[:split_index].reset_index(drop=True), df[split_index:].reset_index(drop=True)
+
+    print('Done')
+
+    return train, test, tokenizer.vocab_size
+    
+
+class SRNN_Dataset(Dataset):
+    def __init__(self, data):
+        """
+        Arguments:
+        
+        data (DataFrame): Columns are ['text', 'tokens', 'labels']
         """
 
-        print('Reading and Preprocessing ... ', end='', flush=True)
-        
-        df = pd.read_csv(filename)
-        df = df.rename(columns={'review': 'text', 'sentiment': 'label'})
-
-        #Replace 'positive' and 'negative' labels with 1 or 0 respectively
-        df['label'] = df['label'].map(lambda x: 1 if x=='positive' else 0)
-
-        print('Done')
-        print('Cleaning ... ', end='', flush=True)
-
-        def clean_sent(sent):
-            import re
-            #HTML Tags
-            sent = re.sub(r"(<.*?>){1,2}", " ", sent)
-            
-            #Nonword characters
-            #sent = re.sub(r"[^\w\s]", "", sent)
-
-            #Removes repeating whitespace
-            sent = re.sub(r"\s+", " ", sent)
-
-            return sent
-            
-        df['text'] = df['text'].apply(clean_sent)
-        #breakpoint()
-        
-        print('Done')
-        print('Tokenizing ... ', end='', flush=True)
-
-        #BERT Tokenizer & Encoding
-        tokenizer = BertTokenizerFast.from_pretrained("bert-base-cased")
-        df = pd.concat([df['text'], df['text'].apply(tokenizer.encode, padding="max_length", truncation=True, return_tensors="pt").rename('tokens'), df['label']], axis=1)
-        df.tokens = df.tokens.map(torch.flatten)
-
-        print('Done')
-        print('Shuffle and Split ... ', end='', flush=True)
-
-        df = df.sample(frac=1, ignore_index=True, random_state=1)
-        split_index = round(len(df)*split_val)
-        train, test = df[:split_index].reset_index(drop=True), df[split_index:].reset_index(drop=True)
-
-        print('Done')
-
-        self.split_val = split_val
-        self.train = train
-        self.test = test
-        self.tokenizer = tokenizer
-        self.vocab_size = tokenizer.vocab_size
+        self.data = data
 
     def __len__(self):
-        return len(self.train.index)
+        return len(self.data.index)
 
     def __getitem__(self, ind):
-        item = self.train.iloc[ind]
+        item = self.data.iloc[ind]
         return item['tokens'], item['label']
 
 class SentimentRNN(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, num_layers, dropout_prob):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, no_layers, dropout_prob):
         super(SentimentRNN, self).__init__()
 
-        #Params
+        # Params
         self.vocab_size = vocab_size
         self.embedding_dim = embedding_dim
         self.hidden_dim = hidden_dim
-        self.num_layers = num_layers
+        self.no_layers = no_layers
         self.dropout_prob = dropout_prob
 
-        #Model Components
+        # Model Components
         self.emb = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers, batch_first=True)
-        self.dropout = nn.Dropout(dropout_prob)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=no_layers, batch_first=True, dropout=dropout_prob)
         self.linear = nn.Linear(hidden_dim, 1)
-        self.relu = nn.ReLU()
+        self.sig = nn.Sigmoid()
 
     def forward(self, batch, hidden, batch_size):
         embeds = self.emb(batch) #Shape [batch_size, 512 (Max from BERT), hidden_dim]
@@ -106,18 +107,14 @@ class SentimentRNN(nn.Module):
         lstm_out, hidden = self.lstm(embeds, hidden) #Types are Tensor, tuple
         lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim) # Shape is now [batch_size * 512, hidden_dim]
 
-        lstm_out = self.dropout(lstm_out)
         lin_out = self.linear(lstm_out) # Shape [batch_size * 512, 1]
 
-        relu_out = self.relu(lin_out) # Shape [batch_size * 512, 1]
-        relu_out = relu_out.view(batch_size, -1)[:, -1]
+        sig_out = self.sig(lin_out) # Shape [batch_size * 512, 1]
+        sig_out = sig_out.view(batch_size, -1)[:, -1]
 
-        return relu_out, hidden
-
+        return sig_out, hidden
 
     def init_hidden(self, batch_size, device):
-        #h0 = torch.zeros((self.no_layers, batch_size, self.hidden_dim)).to(device)
-        #h1 = torch.zeros((self.no_layers, batch_size, self.hidden_dim)).to(device)
-        h0 = torch.zeros((self.num_layers, batch_size, self.hidden_dim))
-        h1 = torch.zeros((self.num_layers, batch_size, self.hidden_dim))
+        h0 = torch.zeros((self.no_layers, batch_size, self.hidden_dim)).to(device)
+        h1 = torch.zeros((self.no_layers, batch_size, self.hidden_dim)).to(device)
         return (h0, h1)
